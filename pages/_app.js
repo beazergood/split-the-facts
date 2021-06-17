044369d6c3928df220af88cb5c5ccc137acb705d
Dave Beazer <dave@sideprojectco.com>
2021-06-17 16:53:38 +0100

    embedded video. updated links

--- pages/_app.js
+++ pages/_app.js
@@ -6,21 +6,6 @@ import '../styles/main.css'
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   )
