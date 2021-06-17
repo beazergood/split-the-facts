6939190d4080ce37ce90138c52763ae2622da9c4
Dave Beazer <dave@sideprojectco.com>
2021-06-17 17:43:29 +0100

    Add image. change h1

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Sam Roffey.
+      {/* <time>{YEAR}</time> © Sam Roffey. */}
       <style jsx>{`
         a {
           float: right;
