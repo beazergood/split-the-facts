711188114bbcd0617dbff70d3dea648af4287a46
Dave Beazer <dave@sideprojectco.com>
2021-07-04 20:06:05 +0100

    purge tailwind. bump to 1.0.3-0

--- next.config.js
+++ next.config.js
@@ -4,7 +4,5 @@ module.exports = {
     MAILCHIMP_API_KEY: '8687287d81abf806bf0c702e94bfca31-us6', // STF
     MAILCHIMP_AUDIENCE_ID: '04a8b4dbd2', // STF
     MAILCHIMP_SERVER_PREFIX: 'us6'
-    // MAILCHIMP_AUDIENCE_ID: '534e46c2fa', // WJ
-    // MAILCHIMP_API_KEY: '1a8e4419ab2cde7ef11fbb5f2f550827-us6' // WJ
   }
 }
