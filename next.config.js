5a702897157d0d6420a9caee53f5f30a128fa69b
Dave Beazer <dave@sideprojectco.com>
2021-07-25 23:03:46 +0100

    correct fix for apollo-client

--- next.config.js
+++ next.config.js
@@ -7,8 +7,7 @@ module.exports = withMDX({
   env: {
     MAILCHIMP_API_KEY: '8687287d81abf806bf0c702e94bfca31-us6', // STF
     MAILCHIMP_AUDIENCE_ID: '04a8b4dbd2', // STF
-    MAILCHIMP_SERVER_PREFIX: 'us6',
-    NEXT_PUBLIC_STRAPI_API_URL: 'https://split-the-facts-test.herokuapp.com'
+    MAILCHIMP_SERVER_PREFIX: 'us6'
   }
 })
 const withImages = require('next-images')
--- scripts/apollo-client.js
+++ scripts/apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+const { NEXT_PUBLIC_STRAPI_API_URL } = process.env
 
 const client = new ApolloClient({
-  uri: process.NEXT_PUBLIC_STRAPI_API_URL,
+  uri: NEXT_PUBLIC_STRAPI_API_URL + '/graphql',
   cache: new InMemoryCache()
 })
 
