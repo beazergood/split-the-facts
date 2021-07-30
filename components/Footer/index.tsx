60e477897a4760240698967ddc9be38e134844ba
Dave Beazer <dave@sideprojectco.com>
2021-07-30 23:12:21 +0100

    fix border breaking build

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -299,14 +299,12 @@ export const Footer: React.FC<FooterProps> = ({ theme, playlist = {} }) => {
               <input
                 type="image"
                 src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif"
-                border="0"
                 name="submit"
                 title="PayPal - The safer, easier way to pay online!"
                 alt="Donate with PayPal button"
               />
               <img
                 alt=""
-                border="0"
                 src="https://www.paypal.com/en_GB/i/scr/pixel.gif"
                 width="1"
                 height="1"
