ea649219c4d1a64534466c905b79cf92472d63a8
Dave Beazer <dave@sideprojectco.com>
2021-08-15 11:34:25 +0100

    move env vars into Vercel

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const withMDX = require('@next/mdx')({
 })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx', 'tsx'],
-  reactStrictMode: true,
-  env: {
-    MAILCHIMP_API_KEY: '8687287d81abf806bf0c702e94bfca31-us6', // STF
-    MAILCHIMP_AUDIENCE_ID: '04a8b4dbd2', // STF
-    MAILCHIMP_SERVER_PREFIX: 'us6'
-  }
+  reactStrictMode: true
 })
 const withImages = require('next-images')
 module.exports = withImages({
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -2,8 +2,8 @@ const mailchimp = require('@mailchimp/mailchimp_marketing')
 const axios = require('axios')
 
 mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_SERVER_PREFIX
+  apiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
+  server: process.env.NEXT_PUBLIC_MAILCHIMP_SERVER_PREFIX
 })
 
 async function run() {
@@ -43,13 +43,13 @@ export default async (req, res) => {
 
 function getRequestParams(email) {
   // get env variables
-  const API_KEY = process.env.MAILCHIMP_API_KEY
-  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID
+  const API_KEY = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY
+  const AUDIENCE_ID = process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID
 
   // get the mailchimp datacenter
   // mailchimp API keys example: c0e214879c8542a54e716f38edf1635d-us2
   // we need the us2 part
-  const DATACENTER = process.env.MAILCHIMP_API_KEY.split('-')[1]
+  const DATACENTER = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY.split('-')[1]
 
   const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`
 
