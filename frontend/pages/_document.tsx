606b984b5c1aee3c727cc90bd709425deebbe876
Dave Beazer <dave@sideprojectco.com>
2021-11-29 20:46:18 +0000

    add script

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -75,7 +75,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9361191567282987"
-            crossorigin="anonymous"
           ></script>
         </Head>
         <body>
